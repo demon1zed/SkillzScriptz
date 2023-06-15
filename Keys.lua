31540a7dfd64b17b3daa22ee7a9499fa0b892eaa
Les <80661322+Lesleeeee@users.noreply.github.com>
2023-06-15 20:22:58 +0800

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -9,10 +9,7 @@ return {
     "kinghedarp", --// 18.06.2023
     "ribissqw", --// 19.06.2023
     "Sherlondobre", --// 20.06.2023
-    "cistianobodado", --// 21.06.2023
-    "Lovnojes", --// LifeTime
     "dev_yaskofafa", --// 31.06.2023
-    "ImNotJaneq", --// 14.07.2023
     "Mouradayogroup", --// 04.07.2023
     "Gents9989" --// 16.06.2023
 }
