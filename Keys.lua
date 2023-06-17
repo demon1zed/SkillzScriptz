bb5d582bcb2bd00025b29cb142b67adb8951ab98
Les <80661322+Lesleeeee@users.noreply.github.com>
2023-06-18 02:17:05 +0800

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -5,11 +5,8 @@ return {
     "Ayd_1010", --// LifeTime
     "beamedbylucidbeams", --// LifeTime
     "arenyoyo1234",
-    "abdulkareem0475", --// 18.06.2023
-    "kinghedarp", --// 18.06.2023
     "ribissqw", --// 19.06.2023
     "Sherlondobre", --// 20.06.2023
     "dev_yaskofafa", --// 31.06.2023
     "Mouradayogroup", --// 04.07.2023
-    "Gents9989" --// 16.06.2023
 }
