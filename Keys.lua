d57062d38239f66f4dedeb5639cb2cde0317fced
Les <80661322+Lesleeeee@users.noreply.github.com>
2023-06-19 23:33:29 +0800

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -3,10 +3,7 @@ return {
     "NxllOwnsYou", 
     "itsrealselimonyx", --//Lifetime
     "Ayd_1010", --// LifeTime
-    "beamedbylucidbeams", --// LifeTime
     "arenyoyo1234",
-    "ribissqw", --// 19.06.2023
-    "Sherlondobre", --// 20.06.2023
     "dev_yaskofafa", --// 31.06.2023
     "Mouradayogroup", --// 04.07.2023
 }
